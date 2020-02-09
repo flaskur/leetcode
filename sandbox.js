61cc8178ab11cfa446c966591e42fb62d81a52a1
flaskur <flaskur@gmail.com>
2020-02-08 23:10:30 -0500

    testing digit access by modulus

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,8 @@
-console.log('hello world');
-let x = 1;
-let y = 2;
-console.log(x + y);
-console.log('messed up formatting');
-
-let wierd = 'some string';
+let x = 18314;
+let result = [];
+while (x > 0) {
+	console.log(`unshifting ${x % 10}`);
+	result.unshift(x % 10);
+	x = Math.floor(x / 10);
+}
+console.log(result);
