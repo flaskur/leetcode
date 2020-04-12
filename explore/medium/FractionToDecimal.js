11a0cec888477d9bb8cfa23f8cb4b44dcd411cfb
flaskur <flaskur@gmail.com>
2020-04-12 10:58:42 -0400

    fraction to decimal repeating

---
+++ explore/medium/FractionToDecimal.js
@@ -0,0 +1,39 @@
+/**
+ * Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+ * 
+ * @param {number} numerator
+ * @param {number} denominator
+ * @return {string}
+ */
+const fractionToDecimal = function(numerator, denominator) {
+	// To avoid if numerator = 0, denominator = 1, then Math.sign(numerator) = 0, Math.sign(denominator) = 1, s = '-0'
+	if (numerator === 0) return '0';
+
+	let s = '';
+	if (Math.sign(numerator) !== Math.sign(denominator)) s += '-';
+
+	let n = Math.abs(numerator);
+	const d = Math.abs(denominator);
+
+	s += Math.floor(n / d);
+	n %= d;
+
+	if (n === 0) return s;
+
+	s += '.';
+
+	const map = {};
+
+	while (n !== 0) {
+		map[n] = s.length;
+
+		n *= 10;
+		s += Math.floor(n / d);
+		n %= d;
+
+		const i = map[n]; // repeat starting index
+		if (i != null) return `${s.slice(0, i)}(${s.slice(i)})`;
+	}
+
+	return s;
+};
