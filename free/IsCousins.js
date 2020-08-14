e24f3a9bd9696da71deeb5aac16f18439a7a4f13
flaskur <flaskur@gmail.com>
2020-08-14 19:37:18 -0400

    blank

---
+++ free/IsCousins.js
@@ -0,0 +1,17 @@
+/**
+ * In a binary tree, the root is depth 0. Two nodes are cousins in a binary tree if they have the same depth but different parents.
+ * Find the depth of each node individually by BFS, and keep track of parent?
+ * BST so you can traverse through tree until you reach the value or node is null.
+ * 
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ * 
+ * @param {TreeNode} root
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+const isCousins = (root, x, y) => {};
