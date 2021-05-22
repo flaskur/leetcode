8c004faa41e6a375425395b114d55003acd2be0a
flaskur <flaskur@gmail.com>
2021-05-22 14:14:15 -0400

    rotated array using partition copy and 3 times reverse

---
+++ golang/rotate.go
@@ -0,0 +1,26 @@
+package main
+
+// given an array, rotate the array to the right by k steps
+// func rotate(nums []int, k int) {
+// 	steps := k % len(nums)
+
+// 	rotatedNums := append(nums[len(nums)-steps:], nums[:len(nums)-steps]...)
+// 	copy(nums, rotatedNums)
+// }
+
+func rotate(nums []int, k int) {
+	steps := k % len(nums)
+
+	// inclusive ranges
+	reverse(nums, 0, len(nums)-1)
+	reverse(nums, 0, steps-1)
+	reverse(nums, steps, len(nums)-1)
+}
+
+func reverse(nums []int, start int, end int) {
+	for start < end {
+		nums[start], nums[end] = nums[end], nums[start]
+		start += 1
+		end -= 1
+	}
+}
