1973a6f0b94e87e5aa286f491efdeb91865c27f0
flaskur <flaskur@gmail.com>
2021-05-22 23:07:28 -0400

    move zeroes and keep relative order using pointers and filling end

---
+++ golang/move_zeroes.go
@@ -0,0 +1,34 @@
+package main
+
+// given an integer array, move all 0's to the end while maintaining relative order of nonzero elements
+// use two pointer method and swap on nonzero occurrence
+// func moveZeroes(nums []int) {
+// 	i := 0
+// 	j := 0
+
+// 	for i < len(nums)-1 && j < len(nums) {
+// 		if nums[j] != 0 {
+// 			nums[i], nums[j] = nums[j], nums[i]
+// 			i += 1
+// 			j += 1
+// 		} else {
+// 			j += 1
+// 		}
+// 	}
+
+// }
+
+// just use one pointer and iterate over array, overwrite end
+func moveZeroes(nums []int) {
+	i := 0
+	for _, num := range nums {
+		if num != 0 {
+			nums[i] = num
+			i += 1
+		}
+	}
+
+	for ; i < len(nums); i++ {
+		nums[i] = 0
+	}
+}
