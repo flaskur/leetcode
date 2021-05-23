4e3dbfa822ba6cb32b93b25afccf59bcf18281d0
flaskur <flaskur@gmail.com>
2021-05-23 15:15:28 -0400

    reverse string in array using two pointers and while loop swap

---
+++ golang/reverse_string.go
@@ -0,0 +1,13 @@
+package main
+
+// given input string as an array of chars, return the reversed string. no extra space done in place.
+func reverseString(s []byte) {
+	i := 0
+	j := len(s) - 1
+
+	for i < j {
+		s[i], s[j] = s[j], s[i]
+		i += 1
+		j -= 1
+	}
+}
