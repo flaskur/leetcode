5d812dccfc2a575669985fa6076e4baa380e5a8b
flaskur <flaskur@gmail.com>
2021-06-07 17:33:26 -0400

    even num digits using strconv and divide 10 loop

---
+++ golang/even_number_digits.go
@@ -0,0 +1,31 @@
+// given an array of integers, return the number of nums that have an even number of digits.
+// func findNumbers(nums []int) int {
+// 	count := 0
+
+// 	for _, num := range nums {
+// 		if len(strconv.Itoa(num))%2 == 0 {
+// 			count++
+// 		}
+// 	}
+
+// 	return count
+// }
+
+func findNumbers(nums []int) int {
+	count := 0
+
+	for _, num := range nums {
+		digits := 0
+
+		for num > 0 {
+			num /= 10
+			digits++
+		}
+
+		if digits%2 == 0 {
+			count++
+		}
+	}
+
+	return count
+}
