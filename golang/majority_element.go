a89eb024c71f91a8194f4f130475d6a16c053e1a
flaskur <flaskur@gmail.com>
2021-06-15 21:43:18 -0400

    fixed for condition

--- golang/majority_element.go
+++ golang/majority_element.go
@@ -18,7 +18,7 @@ func majorityElement(nums []int) int {
 	major := nums[0]
 	count := 1
 
-	for i := 1; len(nums); i++ {
+	for i := 1; i < len(nums); i++ {
 		if count == 0 {
 			major = nums[i]
 			count = 1
