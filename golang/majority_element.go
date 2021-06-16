62dba433e195437113a57a38b5b89fdd7c63bd34
flaskur <flaskur@gmail.com>
2021-06-15 21:42:23 -0400

    majority element using freq map and count major var

---
+++ golang/majority_element.go
@@ -0,0 +1,33 @@
+// given an array of nums, return the majority element.
+func majorityElement(nums []int) int {
+	freq := map[int]int{}
+	for _, num := range nums {
+		freq[num]++
+	}
+
+	for num, count := range freq {
+		if count > len(nums)/2 {
+			return num
+		}
+	}
+
+	return -1
+}
+
+func majorityElement(nums []int) int {
+	major := nums[0]
+	count := 1
+
+	for i := 1; len(nums); i++ {
+		if count == 0 {
+			major = nums[i]
+			count = 1
+		} else if nums[i] == major {
+			count++
+		} else {
+			count--
+		}
+	}
+
+	return major
+}
