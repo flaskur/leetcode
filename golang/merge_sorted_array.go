50fe451ef091ff65d679b20d900efbfcc3ea6cdf
flaskur <flaskur@gmail.com>
2021-06-27 17:04:49 -0400

    added merge without new slice memory

--- golang/merge_sorted_array.go
+++ golang/merge_sorted_array.go
@@ -21,3 +21,25 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	}
 	copy(nums1, nums)
 }
+
+// without extra memory
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	end := m + n - 1
+	i := m - 1
+	j := n - 1
+
+	for i >= 0 && j >= 0 {
+		if nums1[i] > nums2[j] {
+			nums1[end] = nums1[i]
+			i--
+		} else {
+			nums1[end] = nums2[j]
+			j--
+		}
+		end--
+	}
+
+	if j >= 0 {
+		copy(nums1[:end+1], nums2[:j+1])
+	}
+}
