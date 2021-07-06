349cfb2a55a9acf8a90ec8a17999f17e5bd5a4cd
flaskur <flaskur@gmail.com>
2021-07-06 00:33:22 -0400

    sign product array using mod 2 of negative count

---
+++ golang/sign_product_array.go
@@ -0,0 +1,19 @@
+func arraySign(nums []int) int {
+	neg := 0
+	for _, num := range nums {
+		if num == 0 {
+			return 0
+		}
+
+		if num < 0 {
+			neg++
+		}
+	}
+
+	neg = neg % 2
+
+	if neg == 1 {
+		return -1
+	}
+	return 1
+}
