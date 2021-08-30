36e9ff6086927487d6d5106693054e3ed6cd9cb3
flaskur <flaskur@gmail.com>
2021-08-30 19:17:17 -0400

    iterative inorder traversal

--- golang/binary_tree_inorder_traversal.go
+++ golang/binary_tree_inorder_traversal.go
@@ -17,3 +17,26 @@ func helper(node *TreeNode, order *[]int) {
 	// traverse right
 	helper(node.Right, order)
 }
+
+// iterative
+func inorderTraversal(root *TreeNode) []int {
+	order := []int{}
+	stack := []*TreeNode{}
+
+	current := root
+	for current != nil || len(stack) > 0 {
+		for current != nil {
+			stack = append(stack, current) // hold node pointers
+			current = current.Left
+		}
+
+		// pop stack
+		current = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		order = append(order, current.Val)
+		current = current.Right
+	}
+
+	return order
+}
