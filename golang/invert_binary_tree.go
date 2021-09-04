35c236cc0baa1e984e1d4b2f6152aba338583e81
flaskur <flaskur@gmail.com>
2021-09-04 12:23:01 -0400

    invert binary tree iterative solution

--- golang/invert_binary_tree.go
+++ golang/invert_binary_tree.go
@@ -10,3 +10,32 @@ func invertTree(root *TreeNode) *TreeNode {
 
 	return root
 }
+
+// iterative
+func invertTree(root *TreeNode) *TreeNode {
+	// base case
+	if root == nil {
+		return nil
+	}
+
+	stack := []*TreeNode{}
+	stack = append(stack, root)
+	for len(stack) > 0 {
+		// pop
+		current := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		// invert
+		current.Left, current.Right = current.Right, current.Left
+
+		// push children to stack
+		if current.Left != nil {
+			stack = append(stack, current.Left)
+		}
+		if current.Right != nil {
+			stack = append(stack, current.Right)
+		}
+	}
+
+	return root
+}
