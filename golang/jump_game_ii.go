74724cd961306561d6d6003bb364c035ed979085
flaskur <flaskur@gmail.com>
2021-09-14 22:09:22 -0400

    jump game using double loop dp and greedy farthest reach solution

--- golang/jump_game_ii.go
+++ golang/jump_game_ii.go
@@ -23,3 +23,35 @@ func min(x int, y int) int {
 	}
 	return y
 }
+
+// greedy
+func jump(nums []int) int {
+	// idea is that if you reach something, you also reach all elements before it
+	// each iteration should try to find the farthest reach
+	// when you happen upon a previously reached place, move to farthest reached place (which also implies all steps to get there) in one jump
+	if len(nums) == 1 {
+		return 0
+	}
+
+	jumps, reach, prevReach := 0, 0, 0
+
+	for i := 0; i < len(nums); i++ {
+		// find the farthest place you can reach every time
+		if i+nums[i] > reach {
+			reach = i + nums[i]
+		}
+
+		// when i == prevReach, it implies that you have found the farthest reach for the entire range of steps to get there, so new reach will be ideal
+		if i == prevReach {
+			jumps++
+			prevReach = reach
+
+			// end case guaranteed to exist
+			if prevReach >= len(nums)-1 {
+				return jumps
+			}
+		}
+	}
+
+	return -1
+}
