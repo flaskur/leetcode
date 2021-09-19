5ee34c334d88ba2523bf23bb57fa09b2976dca34
flaskur <flaskur@gmail.com>
2021-09-19 11:46:54 -0400

    fix typo

--- golang/daily_temperatures.go
+++ golang/daily_temperatures.go
@@ -4,7 +4,7 @@ func dailyTemperatures(temperatures []int) []int {
 
 	for i := 0; i < len(temperatures); i++ {
 		// keep popping until current temp is no longer greater than top
-		for len(stack) != 0 && temperatures[i] > stack[len(stack)-1] {
+		for len(stack) != 0 && temperatures[i] > temperatures[stack[len(stack)-1]] {
 			index := stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
 			result[index] = i - index
