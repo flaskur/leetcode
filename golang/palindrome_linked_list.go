7411d1148277fde908758f2c65876868e37ed6fe
flaskur <flaskur@gmail.com>
2021-10-10 16:46:03 -0400

    palindrome linked list by using slow fast pointer then reversing right half

--- golang/palindrome_linked_list.go
+++ golang/palindrome_linked_list.go
@@ -26,3 +26,46 @@ func isPalindrome(head *ListNode) bool {
 
 	return true
 }
+
+func isPalindrome(head *ListNode) bool {
+	// idea for this is to use slow fast pointer to find start of right half, reverse right half, then compare
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	// avoid middle if palindrome is odd
+	if fast != nil {
+		slow = slow.Next
+	}
+
+	// slow is now start of right half
+	right := reverse(slow)
+	left := head
+
+	// we are ignoring middle
+	for right != nil {
+		if left.Val != right.Val {
+			return false
+		}
+		left = left.Next
+		right = right.Next
+	}
+
+	return true
+}
+
+func reverse(node *ListNode) *ListNode {
+	var next *ListNode
+	current := node
+	for current != nil {
+		prev := current.Next
+		current.Next = next
+		next = current
+		current = prev
+	}
+
+	// previously the tail
+	return next
+}
