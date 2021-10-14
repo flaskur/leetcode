e6e0fbd69c25d3457c7cecc11ed345bc9526064f
flaskur <flaskur@gmail.com>
2021-10-14 17:17:01 -0400

    group anagram improved using bucket char count string as key instead of sorted word

--- golang/group_anagrams.go
+++ golang/group_anagrams.go
@@ -21,3 +21,34 @@ func sortString(str string) string {
 	sort.Strings(s)
 	return strings.Join(s, "")
 }
+
+func groupAnagrams(strs []string) [][]string {
+	// instead of using the sorted string as a key, use the character counts in bucket sort
+	result := [][]string{}
+	lexico := map[string][]string{}
+
+	for _, str := range strs {
+		buckets := [26]int{}
+		for _, char := range str {
+			buckets[char-'a']++
+		}
+
+		// create key using char count
+		var key strings.Builder
+		for i := 0; i < 26; i++ {
+			key.WriteRune('X') // need separator to discern char count
+			key.WriteString(fmt.Sprint(buckets[i]))
+		}
+
+		// populate anagram groups
+		k := key.String()
+		lexico[k] = append(lexico[k], str)
+	}
+
+	// populate result format
+	for _, words := range lexico {
+		result = append(result, words)
+	}
+
+	return result
+}
