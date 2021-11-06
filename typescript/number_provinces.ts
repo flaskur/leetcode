da2e698b162afc3cbcec4123d2396861edc91eab
flaskur <flaskur@gmail.com>
2021-11-05 23:02:13 -0400

    number provinces using union find

---
+++ typescript/number_provinces.ts
@@ -0,0 +1,48 @@
+function findCircleNum(isConnected: number[][]): number {
+	// init roots (index = value) and rank (optimize find) arrays
+	let roots = [ ...Array(isConnected.length).keys() ]
+	let rank = Array(isConnected.length).fill(1)
+
+	// perform union between on the connected nodes
+	for (let i = 0; i < isConnected.length; i++) {
+		for (let j = 0; j < isConnected[0].length; j++) {
+			if (isConnected[i][j] == 0) continue
+			union(roots, rank, i, j)
+		}
+	}
+
+	// find all unique nums in roots
+	let seen = new Set<number>()
+	roots.forEach(root => {
+		let realRoot = find(roots, root)
+		seen.add(realRoot)
+	})
+
+	return seen.size
+}
+
+function find(roots: number[], x: number) {
+	// continue to traverse up tree until value matches index => root
+	if (x == roots[x]) return x
+	return find(roots, roots[x])
+}
+
+function union(roots: number[], rank: number[], x: number, y: number) {
+	let rootX = find(roots, x)
+	let rootY = find(roots, y)
+
+	// connect roots if they are not the same
+	if (rootX != rootY) {
+		// assigning root my rank to avoid linked list traversal
+		if (rank[rootX] > rank[rootY]) {
+			roots[rootY] = rootX // this only updates 1 it doesn't update all
+			// this needs to run find over all of them again
+		} else if (rank[rootX] < rank[rootY]) {
+			roots[rootX] = rootY
+		} else {
+			// arbitrary so just choose one to be higher
+			roots[rootY] = rootX
+			rank[rootX]++
+		}
+	}
+}
