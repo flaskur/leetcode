5cae064b3b695cbbceee53b1c4ea89f218dd6b56
flaskur <flaskur@gmail.com>
2021-11-09 11:27:34 -0500

    reverse integer without type conversion

--- typescript/reverse_integer.ts
+++ typescript/reverse_integer.ts
@@ -9,3 +9,22 @@ function reverse(x: number): number {
 
 	return isNegative ? -num : num
 }
+
+// without type conversion
+function reverse(x: number): number {
+	let isNegative = x < 0
+	let num = 0
+
+	x = Math.abs(x)
+	while (x > 0) {
+		// num shifts left and takes last digit of num => reversing
+		num = 10 * num + x % 10
+		x = ~~(x / 10) // flooring
+	}
+
+	// check bound
+	if (isNegative && num > Math.pow(2, 31)) return 0
+	if (!isNegative && num > Math.pow(2, 31) - 1) return 0
+
+	return isNegative ? -num : num
+}
